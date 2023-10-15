bdb0581dffa25423cf6a5295e95154760eedf141
Ed Khalid <ed.khalid1@gmail.com>
2023-10-14 22:32:28 -0400

    first cut

--- src/App.tsx
+++ src/App.tsx
@@ -2,32 +2,29 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Player } from './models/Player'
+import { Team, TeamColor } from './models/Team'
+import { initialPlayers } from './models/initialState'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [players, setPlayers] = useState<Array<Player>>(initialPlayers)
+  const [teamBlack, setTeamBlack] = useState<Team>(new Team(TeamColor.BLACK, []))
+  const [teamWhite, setTeamWhite] = useState<Team>(new Team(TeamColor.WHITE, []))
+  const [teamRed, setTeamRed] = useState<Team>(new Team(TeamColor.RED, []))
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <ul>
+          { players.map(it => <li key={'free-player-' + it.id}> {it.name}</li>)}
+        </ul>
       </div>
-      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
